b3036d72c51516b74efc3c5e5fc69074b085360e
doosemavis <moosedavis2011@gmail.com>
2020-11-26 10:39:35 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root 'welcome#home'
-  get'welcome/home' => 'welcome#home'
-  get'/auth/facebook/callback' => 'sessions#create'
-  
+  get 'welcome/home' => 'welcome#home'
+  get '/auth/facebook/callback' => 'sessions#create'
+
 end
